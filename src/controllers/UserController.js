16c4b81f1e2e620230baa4de8ad8b2b14f6d8db9
Mahesh0843 <singulurimahesh123@gmail.com>
2024-12-10 02:45:31 +0530

    feed_Updated

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -12,7 +12,7 @@ exports.getRequests=async (req,res)=>{
         }).populate("fromUserId", USER_SAFE_DATA);
         if(!connRequest)
         {
-            throw new Error("user not found with interested");
+          throw new Error("user not found with interested");
         }
         res.status(200).json({
             message:"Data fetched Successfully",
@@ -54,3 +54,36 @@ exports.getconnections=async (req,res)=>{
 
     }
 };
+
+exports.getfeed=async (req,res)=>{
+  try{
+    const { page = 1, limit = 10} = req.query;
+    const loggedInUser = req.user;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId  toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequests.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({ data: users });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,8 @@
 const express=require("express");
 const userRouter=express.Router();
 const {userAuth}=require("../middleware/auth");
-const {getRequests, getconnections}=require("../controllers/UserController");
+const {getRequests, getconnections,getfeed}=require("../controllers/UserController");
 userRouter.get("/user/requests/received",userAuth,getRequests);
 userRouter.get("/user/getConnections",userAuth,getconnections);
+userRouter.get("/user/feed",userAuth,getfeed);
 module.exports=userRouter;
